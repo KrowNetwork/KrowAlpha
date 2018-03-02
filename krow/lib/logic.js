a5a13f16d763a145700f34a9f3987cc87cb9f091
Krow <siegel.tucker@gmail.com>
2018-03-01 22:00:08 -0500

    add print for test

--- krow/lib/logic.js
+++ krow/lib/logic.js
@@ -58,6 +58,7 @@ function addResume(addResume) {
 function addResume(addEducation) {
   // adds resume in the transaction to the user
   var eds = new Array()
+  console.log(addEducation.resume.education)
   if (addEducation.resume.education == Null) {
     addEducation.resume.education = new Array();
   }
