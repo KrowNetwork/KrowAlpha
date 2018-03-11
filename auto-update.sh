1073cf04b390b4dc1becb00b34b56758acf1e190
Krow <siegel.tucker@gmail.com>
2018-03-11 15:35:43 -0400

    update auto updater script

--- auto-update.sh
+++ auto-update.sh
@@ -6,4 +6,4 @@ composer archive create -t dir -n .
 
 composer network update -c admin@krow -a krow@0.0.1.bna
 
-composer-rest-server -c admin@krow -n never -w true
+composer-rest-server -c admin@krow -n never -w true -a true
