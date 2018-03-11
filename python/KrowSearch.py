dc3fff169881081326839dd9979b23013388938b
Krow <siegel.tucker@gmail.com>
2018-03-10 22:11:30 -0500

    update search class

--- python/KrowSearch.py
+++ python/KrowSearch.py
@@ -9,13 +9,23 @@ class KrowSearch(object):
         pass
 
     class search_user(object):
-        
+
+        @staticmethod
+        def all():
+            url = "http://18.220.46.51:3000/api/queries/getAllUsers"
+            r = requests.get(url)
+            if r.status_code == 200:
+                return r.text
+            else:
+                print ("Error %s" % r.status_code)
+                print (r.text)
+
         @staticmethod
         def by_email(email=""):
             url = "http://18.220.46.51:3000/api/queries/getUserByEmail?email=%s" % email
             r = requests.get(url)
             if r.status_code == 200:
-                print (r.text)
+                return r.text
             else:
                 print ("Error %s" % r.status_code)
                 print (r.text)
