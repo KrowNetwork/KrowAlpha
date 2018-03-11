1e15a3bfc2a16bffeecb8aae627ab6166404ada6
Krow <siegel.tucker@gmail.com>
2018-03-10 19:34:20 -0500

    create python files

---
+++ python/test.py
@@ -0,0 +1,8 @@
+from Krow import krow
+
+krow = krow()
+
+krow.create_user(first_name="Tucker",
+                 last_name="Siegel",
+                 email="siegel.tucker@gmail.com",
+                 birthday="02/05/2001")
