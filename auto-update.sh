4ab773f7790e9805cb07cb44f57ed257af3ad6d3
Krow <siegel.tucker@gmail.com>
2018-03-11 21:16:24 -0400

    remove github auth

--- auto-update.sh
+++ auto-update.sh
@@ -8,4 +8,4 @@ composer archive create -t dir -n .
 
 composer network update -c admin@krow -a krow@0.0.1.bna
 
-composer-rest-server -c admin@krow -n never -w true -a true
+composer-rest-server -c admin@krow -n never -w true
