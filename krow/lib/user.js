c9ec46f2da05cd556f966aff1341a4bf57c3102f
Krow <siegel.tucker@gmail.com>
2018-03-13 19:37:30 -0400

    testing new json datafield

---
+++ krow/lib/user.js
@@ -0,0 +1,25 @@
+/**
+ * @param {org.krow.transactions.UpdateUserEducation} updateUserEducation - updateUserEducation to be processed
+ * @transaction
+ */
+function UpdateUserEducation(updateUserEducation) {
+  // format: {degree: DEGREE, school: SCHOOL, details: DETAILS}
+
+  var factory = getFactory();
+
+  // get json to add
+  var jsonText = updateUserEducation.user.resume.education;
+  //convert to json
+  jsonText = JSON.parse(jsonText);
+  // push to json
+  jsonText.push(updateUserEducation.newJson);
+  // update
+  updateUserEducation.user.resume.education = JSON.stringify(jsonText);
+  // update the resume
+  return getAssetRegistry('org.krow.assets.Resume')
+  		.then(function (assetRegistry) {
+    		return assetRegistry.update(updateUserEducation.user.resume);
+
+  })
+
+}
