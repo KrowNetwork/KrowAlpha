8c7b60650afe0558e9cb1e5c1a9b7bf9a92a1c26
unknown <siegel.tucker@gmail.com>
2018-03-17 22:24:52 -0700

    update js with new function and fix error

--- krow/lib/applicant.js
+++ krow/lib/applicant.js
@@ -13,9 +13,40 @@
    event.resume = resume;
    emit(event);
 
-   return getAssetRegistry('network.krow.participants.Applicant')
-   		.then(function (assetRegistry) {
-     		return assetRegistry.update(applicant);
+   return getParticipantRegistry('network.krow.participants.Applicant')
+   		.then(function (participantRegistry) {
+     		return participantRegistry.update(applicant);
 
    })
  }
+
+
+ /**
+  * @param {network.krow.transactions.applicant.RequestJob} requestJob - requestJob to be processed
+  * @transaction
+  */
+  function RequestJob(requestJob) {
+    var factory = getFactory(); // get factory to emit events
+    var employer = requestJob.employer;
+    var applicant = requestJob.applicant;
+    var job = requestJob.job;
+
+    if (jpb.hasApplicants == false) {
+      job.applicantRequests = new Array();
+      job.hasApplicants == true;
+    }
+
+    var applicantRef = factory.newRelationship("network.krow.participants", "Applicant", applicant.applicantID);
+    job.applicantRequests.push(applicantRef);
+
+    var event = factory.newEvent("network.krow.transactions.applicant", "ResumeChangedEvent");
+    event.applicant = applicant;
+    event.resume = resume;
+    emit(event);
+
+    return getAssetRegistry('network.krow.assets.Job')
+    		.then(function (assetRegistry) {
+      		return assetRegistry.update(job);
+
+    })
+  }
