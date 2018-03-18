b593b640a4a7c6130de4ff52472d75cee0c848c5
unknown <siegel.tucker@gmail.com>
2018-03-17 22:32:54 -0700

    add new function

--- krow/lib/applicant.js
+++ krow/lib/applicant.js
@@ -26,12 +26,12 @@
   * @transaction
   */
   function RequestJob(requestJob) {
-    var factory = getFactory(); // get factory to emit events
+    var factory = getFactory(); // get factory to emit events and create relationships
     var employer = requestJob.employer;
     var applicant = requestJob.applicant;
     var job = requestJob.job;
 
-    if (jpb.hasApplicants == false) {
+    if (job.hasApplicants == false) {
       job.applicantRequests = new Array();
       job.hasApplicants == true;
     }
@@ -51,3 +51,41 @@
 
     })
   }
+
+/**
+ * @param {network.krow.transactions.applicant.UnrequestJob} unrequestJob - unrequestJob to be processed
+ * @transaction
+ */
+ function UnrequestJob(unrequestJob) {
+   var factory = getFactory(); // get factory to emit events and create relationships
+   var employer = requestJob.employer;
+   var applicant = requestJob.applicant;
+   var job = requestJob.job;
+
+   if (job.hasApplicants == false) {
+     return;
+   }
+
+   for (int i = 0; i < job.applicantRequests.length; i ++) {
+     if (job.applicantRequests[i].applicantID === applicant.applicantID) {
+       job.applicantRequests.splice(i, 1);
+     }
+   }
+
+
+   if (job.applicantRequests.length == 0) {
+     job.hasApplicants = false;
+   }
+
+   var event = factory.newEvent("network.krow.transactions.applicant", "UnrequestJobEvent");
+   event.employer = employer;
+   event.applicant = applicant;
+   event.job = job;
+   emit(event);
+
+   return getAssetRegistry('network.krow.assets.Job')
+       .then(function (assetRegistry) {
+         return assetRegistry.update(job);
+
+   })
+ }
