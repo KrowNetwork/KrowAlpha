721a1be2c18c88d3d7590dcd0f635cde36d01c33
unknown <siegel.tucker@gmail.com>
2018-03-17 23:01:33 -0700

    add first employer function

---
+++ krow/lib/employer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {network.krow.transactions.employer.NewJob} newJob - NewJob to be processed
+ * @transaction
+ */
+ function NewJob(newJob) {
+   var factory = getFactory(); // get factory to emit events
+   var employer = newJob.employer;
+   var job = newJob.job;
+
+   if (employer.hasAvailableJobs == false) {
+     employer.availableJobs = new Array();
+     employer.hasAvailableJobs == true;
+   }
+
+   var jobRef = factory.newRelationship("network.krow.assets", "Job", job.jobID);
+   employer.availableJobs.push(jobRef);
+
+   return getParticipantRegistry('network.krow.participants.Employer')
+   		.then(function (participantRegistry) {
+     		return participantRegistry.update(employer);
+
+   })
+      .then(function () {
+        var event = factory.newEvent("network.krow.transactions.applicant", "ResumeChangedEvent");
+        event.applicant = applicant;
+        event.resume = resume;
+        emit(event);
+      })
+ }
