e9cb693bf592e289fa8e9ceea9719b2d9327e33d
unknown <siegel.tucker@gmail.com>
2018-03-18 02:48:23 -0700

    add remove job transaction

--- krow/lib/employer.js
+++ krow/lib/employer.js
@@ -27,3 +27,33 @@
         emit(event);
       })
  }
+
+/**
+* @param {network.krow.transactions.employer.RemoveJob} removeJob - RemoveJob to be processed
+* @transaction
+*/
+function RemoveJob(removeJob) {
+  var factory = getFactory(); // get factory to emit events
+  var employer = removeJob.employer;
+  var job = removeJob.job;
+
+
+
+  for (var i = 0; i < employer.availableJobs.length; i ++) {
+    if (employer.availableJobs[i].jobID == job.jobID) {
+      employer.availableJobs.split(i, 1);
+    }
+  }
+
+  return getParticipantRegistry('network.krow.participants.Employer')
+  		.then(function (participantRegistry) {
+    		return participantRegistry.update(employer);
+
+  })
+     .then(function () {
+       var event = factory.newEvent("network.krow.transactions.employer", "JobRemovedEvent");
+       event.employer = employer;
+       event.job = job;
+       emit(event);
+     })
+}
