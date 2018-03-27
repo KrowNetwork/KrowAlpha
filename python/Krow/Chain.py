c4b293c08c39a2475fc32cf614c2d6ecc41e9871
unknown <siegel.tucker@gmail.com>
2018-03-27 19:30:44 -0400

    add JSONError

--- python/Krow/Chain.py
+++ python/Krow/Chain.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from Krow.applicant import Applicant
+from Krow.errors import JSONError
 
 class Chain(object):
 
@@ -10,6 +11,8 @@ class Chain(object):
 
     def get_applicant(self, applicantID):
         r = self.session.get("%sapi/Applicant/%s" % (self.url, applicantID))
+        if r.status_code != 200:
+            raise JSONError("Status code %s returned. Json returned: \n\n%s" % (r.status_code, r.text))
         applicant_json = json.loads(json.dumps(r.json()))
         return Applicant(json=applicant_json)
 
--- python/Krow/__init__.py
+++ python/Krow/__init__.py
@@ -1,2 +1,3 @@
 from Krow.chain import Chain
 from Krow.applicant import Applicant
+from Krow.errors import *
