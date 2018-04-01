a0dda7eb3dbb41a73e74b1b143b99fa367434308
unknown <siegel.tucker@gmail.com>
2018-04-01 02:04:27 -0400

    remove certain comments

--- python/test_suite.py
+++ python/test_suite.py
@@ -31,7 +31,6 @@ def request_unrequest(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST UNREQUEST'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     applicant.unrequest_job(chain, employer, job); print ("unrequested")   #WORKS
 
@@ -43,7 +42,6 @@ def request_deny(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST DENY'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.deny_applicant(chain, applicant, job); print ("denied")   #WORKS
 
@@ -55,7 +53,6 @@ def request_deny_request(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST DENY REQUEST'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.deny_applicant(chain, applicant, job); print ("denied")   #WORKS
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS - SHOULD RETURN ERROR
@@ -68,7 +65,6 @@ def request_hire_resign(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE RESIGN'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.resign_job(chain, employer, job); print ("resigned")    #WORKS
@@ -81,7 +77,6 @@ def request_hire_resign_fire(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE RESIGN FIRE'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.resign_job(chain, employer, job); print ("resigned")    #WORKS
@@ -95,7 +90,6 @@ def request_hire_applicant_complete(chain):
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE APPLICANT COMPLETE'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.complete_job(chain, employer, job); print ("completed")    #NOT WRITTEN IN CODE
