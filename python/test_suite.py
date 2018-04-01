560d0ea5485d1d39a429cd4ce6ec9d2b8944ea11
unknown <siegel.tucker@gmail.com>
2018-04-01 01:54:23 -0400

    update

--- python/test_suite.py
+++ python/test_suite.py
@@ -10,87 +10,85 @@ def clear(chain):
     chain.put(job)
     employer.post_job(chain, job)
 
-def request_hire_fire(chain):
+def request_hire_fire(chain):   '''PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE FIRE    PASS'''
+    '''REQUEST HIRE FIRE'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     employer.fire_applicant(chain, applicant, job); print ("fired")    #WORKS
 
-def request_unrequest(chain):
+def request_unrequest(chain):   '''PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST UNREQUEST    PASS'''
+    '''REQUEST UNREQUEST'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     applicant.unrequest_job(chain, employer, job); print ("unrequested")   #WORKS
 
-def request_deny(chain):
+def request_deny(chain):    '''PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST DENY     PASS'''
+    '''REQUEST DENY'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.deny_applicant(chain, applicant, job); print ("denied")   #WORKS
 
-def request_deny_request(chain):
+def request_deny_request(chain):    '''SEMI-PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST DENY REQUEST     PASS'''
+    '''REQUEST DENY REQUEST'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.deny_applicant(chain, applicant, job); print ("denied")   #WORKS
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS - SHOULD RETURN ERROR
 
-def request_hire_resign(chain):
+def request_hire_resign(chain):    '''PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE RESIGN      PASS'''
+    '''REQUEST HIRE RESIGN'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.resign_job(chain, employer, job); print ("resigned")    #WORKS
 
-def request_hire_resign_fire(chain):
+def request_hire_resign_fire(chain):    '''SEMI-PASS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE RESIGN FIRE     SEMI-PASS'''
+    '''REQUEST HIRE RESIGN FIRE'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.resign_job(chain, employer, job); print ("resigned")    #WORKS
     employer.fire_applicant(chain, applicant, job); print ("fired")    #WORKS SHOULD THROW ERROR BECAUSE THERE IS NO ONE TO FIRE
 
-def request_hire_applicant_complete(chain):
+def request_hire_applicant_complete(chain):    '''FAILS'''
     clear(chain); print ('Cleared')
 
     applicant = chain.get_applicant("SAMPLEAPPLICANT"); print ('Got Applicant From Chain')
     employer = chain.get_employer("SAMPLEEMPLOYER"); print ('Got Employer From Chain')
     job = chain.get_job("SAMPLEJOB"); print ('Got Job From Chain')
 
-    '''REQUEST HIRE APPLICANT COMPLETE      FAILS'''
+    '''REQUEST HIRE APPLICANT COMPLETE'''
     applicant.request_job(chain, employer, job); print ("requested")   #WORKS
     employer.hire_applicant(chain, applicant, job); print ("hired")    #WORKS
     applicant.complete_job(chain, employer, job); print ("completed")    #NOT WRITTEN IN CODE
-
-chain = Chain("http://18.220.46.51:3000/")
