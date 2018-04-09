921ec3bad31cc751cddeef9ad6b80d3ab01113c6
Tucker Siegel <siegel.tucker@gmail.com>
2018-04-09 18:19:55 -0400

    update to 1.1

--- scripts/composer/install.sh
+++ scripts/composer/install.sh
@@ -7,10 +7,10 @@ installdir="/home/ubuntu/composer/"
 
 #"$installdir"/prereqs-ubuntu.sh
 
-npm install -g composer-cli@0.16.6
-npm install -g composer-rest-server@0.16.6
-npm install -g generator-hyperledger-composer@0.16.6
-npm install -g yo@0.16.6
+npm install -g composer-cli
+npm install -g composer-rest-server
+npm install -g generator-hyperledger-composer
+npm install -g yo
 
 #required, but not in hyperledger composer documentation
 npm install -g grpc
@@ -24,7 +24,7 @@ sudo apt-get install -y unzip
 #download fabric-tools
 mkdir "$installdir"/fabric-tools
 cd "$installdir"/fabric-tools
-curl -O  https://raw.githubusercontent.com/hyperledger/composer-tools/4272c13ed4a96d8360a4b4d372c19b87ba5098ec/packages/fabric-dev-servers/fabric-scripts/hlfv1/downloadFabric.sh
+curl -O  https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-scripts/hlfv11/downloadFabric.sh
 unzip -q fabric-dev-servers.zip
 sudo ./downloadFabric.sh
 
